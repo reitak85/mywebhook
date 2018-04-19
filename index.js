1003d9c3fb81171ae02cf0b9575379a41f0a6575
Reinaldo Ueno Takahashi <reinaldot@fourway.com.br>
2018-04-19 16:16:34 -0300

    Initial Commit

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 var routes = require('./API/Routes/Routes'); //importing route
 routes(server); //register the route
-server.listen((8000), function () {
+server.listen((process.env.PORT || 8000), function () {
     console.log("Server is up and listening on port" + process.env.PORT);
 });
